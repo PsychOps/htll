d785e72622779cbe15597be89443cfa71113d302
joshua <joshuaslui0203@gmail.com>
2021-08-29 14:00:06 +0200

    contact.html > contact.php

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+
+    <link rel="icon"
+          type="image/png"
+          href="images/Yulius.png">
+
+    <title>Esquire home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<?php include 'templates/header.php';?>
+
+<section class="container">
+    <section class="container">
+        <script type="text/javascript" defer src="https://form.123formbuilder.com/embed/5916855.js" data-role="form" data-default-width="650px"></script>
+    </section>
+</section>
+</body>
+<?php include 'templates/footerscroll.php';?>
+</html>
