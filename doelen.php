03aae82605a1e9d6c88dceeb4aed21ed6abb4911
joshua <joshuaslui0203@gmail.com>
2021-08-29 15:46:55 +0200

    add doelen.php

---
+++ doelen.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+
+    <link rel="icon"
+          type="image/png"
+          href="images/Yulius.png">
+
+    <title>htlr contact</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php include 'templates/header.php';?>
+
+</body>
+<?php include 'templates/footerstatic.php';?>
+</html>
