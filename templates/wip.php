2955d3e9daa602778048c175fd5be132c25e2e3f
joshua <joshuaslui0203@gmail.com>
2021-08-29 16:09:56 +0200

    Add template wip.php

---
+++ templates/wip.php
@@ -0,0 +1,8 @@
+<section class="container has-text-centered">
+    <h2 class="title h2">Er wordt momenteel nog gewerkt aan deze pagina. Kom later terug!</h2>
+    <a class="button button-dark hvr-grow">Back to home</a>
+    <hr>
+    <p>Als u denkt dat dit een fout is, willen wij u graag verzoeken ons hierover te informeren. Er zal dan zo snel mogelijk naar gekeken worden.<br/>
+        <b><a href="mailto:joshuaslui0203@gmail.com">Neem hier contact op.</a></b>
+    </p>
+</section>
