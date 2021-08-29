7a9fa21160b2f7efbe7ab9260219d3a9d0b52ce3
joshua <joshuaslui0203@gmail.com>
2021-08-29 14:00:25 +0200

    footer.php > footerstatic.php

--- templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footerstatic">
-<?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " Yulius Het Tij</p>";
-?>
-</footer>
---
+++ templates/footerstatic.php
@@ -0,0 +1,5 @@
+<footer class="footerstatic">
+<?php
+echo "<p>Copyright &copy; " . date("Y") . " Yulius Het Tij</p>";
+?>
+</footer>
