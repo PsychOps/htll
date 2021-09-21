8c3a047d3594f807100497d8bc0cdcf087daaf8f
Joshua <joshuaslui0203@gmail.com>
2021-09-21 10:34:15 +0200

    Add Modraxis

--- templates/footerscroll.php
+++ templates/footerscroll.php
@@ -1,5 +1,9 @@
 <footer class="footerscroll">
 <?php
-echo "<p>Copyright &copy; " . date("Y") . " Yulius Het Tij</p>";
+echo "<p>
+Copyright &copy; " . date("Y") . " Yulius Het Tij
+<br/>
+Maintained & developed by <a href="https://modraxis.tk">Modraxis Development</a>
+</p>";
 ?>
 </footer>
