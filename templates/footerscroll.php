408b7bab794fadccaf89c1a7d26a5d75ad82daae
Joshua <joshuaslui0203@gmail.com>
2021-09-21 10:37:52 +0200

    Fix footer

--- templates/footerscroll.php
+++ templates/footerscroll.php
@@ -1,9 +1,5 @@
 <footer class="footerscroll">
 <?php
-echo "<p>
-Copyright &copy; " . date("Y") . " Yulius Het Tij
-<br/>
-Maintained & developed by <a href="https://modraxis.tk">Modraxis Development</a>
-</p>";
+echo "<p>Copyright &copy; " . date("Y") . " Yulius Het Tij<br/>Maintained & developed by <a href="https://modraxis.tk">Modraxis Development</a></p>";
 ?>
 </footer>
