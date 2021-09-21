48675d1a2a6d4672f1b82f45e374d6c92b72343b
Joshua <joshuaslui0203@gmail.com>
2021-09-21 10:41:17 +0200

    Add Modraxis

--- templates/footerstatic.php
+++ templates/footerstatic.php
@@ -1,5 +1,5 @@
 <footer class="footerstatic">
 <?php
-echo "<p>Copyright &copy; " . date("Y") . " Yulius Het Tij</p>";
+echo "<p>Copyright &copy; " . date("Y") . " Yulius Het Tij<br/>Maintained & developed by Modraxis Development</p>";
 ?>
 </footer>
